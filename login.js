e5df400cca7aa3085babb4c629b050ab333e2f23
akbarmuharramgit <137644007+akbarmuharramgit@users.noreply.github.com>
2023-07-10 05:38:20 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+ const correctEmail = 'jwd@gmail.com';
+ const correctPassword ='1234';
+
+ const messageContainer = document.getElementById('messageContainer');
+
+ function cekLogin(){
+ 	const userEmail = document.getElementById('fEmail').value;
+ 	const userPassword = document.getElementById('fPassword').value;
+
+
+ 	if (userEmail != correctEmail || userPassword != correctPassword){
+ 		messageContainer.innerHTML = 
+ 		"<p class='error-message'>Login Gagal, Email atau Password Salah !</p>";
+ 	} else {
+ 		messageContainer.innerHTML = 
+ 		"<p class='succes-message'>Login Berhasil...</p>";
+ 	}
+ 	//alert(userPassword);
+ }
